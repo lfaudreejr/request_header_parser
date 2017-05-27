105cce63030246dbfb802ef2bf3981c638cf046b
Larry Faudree <lfaudrejr@hotmail.com>
2017-05-26 23:02:32 -0400

    moved index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    app = express();
+
+
+app.get('/', function(req, res) {
+    var ipAddress = req.ip,
+        language = req.headers['accept-language'],
+        userAgent = req.headers['user-agent'].match(/\((.+\d)\)/),
+        userOS = userAgent[0].substring(1, userAgent[0].length-1);
+        resObj = {
+            ipaddress: ipAddress,
+            language: language.split(',')[0],
+            software: userOS
+        };
+    res.send(JSON.stringify(resObj));
+});
+
+
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
+    console.log("Listening...");
+});
